7d71a34bfb814e4a6f74eaf8f73971eb7042fbd7
Ajeng Nursyifa <ajengnursyifa18@gmail.com>
2024-10-23 12:53:55 +0700

    tugas

--- 21. Deployment/Praktikum/src/App.jsx
+++ 21. Deployment/Praktikum/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="max-w-md mx-auto p-4 mt-6 bg-pink-200 rounded-xl shadow-md">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Semuaaa!!!</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Hello Gayss!!!</h1>
       <p className="text-lg text-gray-700">
         Ini adalah Web untuk contoh tugas Deployment
       </p>
